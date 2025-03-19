f32c46aed0ca61188503efcff010f020d772366d
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-03-19 16:14:56 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,43 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int i, j, t, n;
+bool scambio;
+cout << "Quante cifre devono essere generate?" << endl;
+cin n;
+vector <int> h[n];
+i=0;
+scambio=true;
+while(i<n)
+{
+   h[i]= rand()n*10;
+   i=i+1;
+}
+i=0;
+while (i<n and scambio=true)
+{
+   scambio=false;
+   j=0;
+   while (j<=n-i)
+   {
+   if(h[j]>h[j+1])
+   {
+    t=h[j];
+    h[j]=h[j+1];
+    h[j+1]=t;
+    scambio=true;
+   }
+   j=j+1;
+   }
+   i=i+1;
+}
+i=0;
+while(i<n)
+{
+    cout << h[i];
+    i=i+1;
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+}
+
+
